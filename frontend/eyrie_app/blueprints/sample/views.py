0f0673b3aee2c07ad5f73fb7828d12dbc4c5cb61
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-10-01 22:45:16 +0200

    Enhance sample detail view with three-tab navigation

--- frontend/eyrie_app/blueprints/sample/views.py
+++ frontend/eyrie_app/blueprints/sample/views.py
@@ -3,5 +3,5 @@ from flask import Blueprint, send_file
 bp = Blueprint('sample', __name__, url_prefix='')
 
 @bp.route("/sample/<sample_id>")
-def sample_detail(sample_id):
-    return send_file('blueprints/sample/templates/detail.html')
+def sample(sample_id):
+    return send_file('blueprints/sample/templates/sample.html')
