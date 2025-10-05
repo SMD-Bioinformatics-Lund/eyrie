903ff97cee2d097cdfcc891b951d869f1aa54d21
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-10-05 12:56:14 +0200

    Implement sample-specific routing architecture

--- frontend/eyrie_app/blueprints/sample/views.py
+++ frontend/eyrie_app/blueprints/sample/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, render_template
 
-bp = Blueprint('sample', __name__, url_prefix='')
+bp = Blueprint('sample', __name__, url_prefix='', template_folder='templates')
 
 @bp.route("/sample/<sample_id>")
 def sample(sample_id):
-    return send_file('blueprints/sample/templates/sample.html')
+    return render_template('sample.html', sample_id=sample_id)
+
+@bp.route("/sample/<sample_id>/classification")
+def sample_classification(sample_id):
+    return render_template('sample_classification.html', sample_id=sample_id)
+
+@bp.route("/sample/<sample_id>/nanoplot")
+def sample_nanoplot(sample_id):
+    return render_template('sample_nanoplot.html', sample_id=sample_id)
