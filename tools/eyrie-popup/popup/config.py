cb60f3fe19dc7cd834a024a9dc459bdae59940e1
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-10-16 18:07:46 +0200

    Add spike detection configuration and version management

---
+++ tools/eyrie-popup/popup/config.py
@@ -0,0 +1,13 @@
+"""Configuration for spike species detection."""
+
+# List of possible spike hit species names
+# These species will be automatically flagged as spike controls during processing
+SPIKE = [
+    "Bacillus subtilis",
+    "Saccharomyces cerevisiae",
+    "Enterococcus faecalis",
+    "Staphylococcus epidermidis",
+    "Lactobacillus plantarum",
+    "Pseudomonas aeruginosa",
+    "Candida albicans"
+]
