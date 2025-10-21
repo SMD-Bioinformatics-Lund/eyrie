31e120109e2106c959f371da0d780e832e4df2e3
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-10-21 16:50:42 +0200

    Apply authentication decorators to all view routes

--- frontend/eyrie_app/blueprints/samples/views.py
+++ frontend/eyrie_app/blueprints/samples/views.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, send_file
+from ...auth.decorators import login_required
 
-bp = Blueprint('samples', __name__, url_prefix='')
+bp = Blueprint('samples', __name__, url_prefix='', template_folder='templates')
 
 @bp.route("/")
-def root():
-    return send_file('blueprints/samples/templates/index.html')
+@login_required
+def root(current_user=None):
+    return render_template('index.html', current_user=current_user)
 
 @bp.route("/samples")
-def samples_page():
-    return send_file('blueprints/samples/templates/index.html')
+@login_required
+def samples_page(current_user=None):
+    return render_template('index.html', current_user=current_user)
