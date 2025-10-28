249eece4b8d53cd5179868664d8f48988552db15
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-10-28 16:56:32 +0100

    Update environment config and remaining template improvements

--- frontend/eyrie_app/blueprints/samples/views.py
+++ frontend/eyrie_app/blueprints/samples/views.py
@@ -1,14 +1,14 @@
-from flask import Blueprint, render_template, send_file
-from ...auth.decorators import login_required
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 bp = Blueprint('samples', __name__, url_prefix='', template_folder='templates')
 
 @bp.route("/")
 @login_required
-def root(current_user=None):
-    return render_template('index.html', current_user=current_user)
+def root():
+    return render_template('samples.html')
 
 @bp.route("/samples")
 @login_required
-def samples_page(current_user=None):
-    return render_template('index.html', current_user=current_user)
+def samples_page():
+    return render_template('samples.html')
