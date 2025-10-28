09e4458bf285ceb0ba3f17a6f46f2d0180c6ec7a
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-10-28 16:47:11 +0100

    Rename sample.html to sample_overview.html and create reusable macros

--- frontend/eyrie_app/blueprints/sample/views.py
+++ frontend/eyrie_app/blueprints/sample/views.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, render_template
-from ...auth.decorators import login_required
+from flask_login import login_required, current_user
 
 bp = Blueprint('sample', __name__, url_prefix='', template_folder='templates')
 
 @bp.route("/sample/<sample_id>")
 @login_required
-def sample(sample_id, current_user=None):
-    return render_template('sample.html', sample_id=sample_id, current_user=current_user)
+def sample_overview(sample_id):
+    return render_template('sample_overview.html', sample_id=sample_id, current_user=current_user)
 
 @bp.route("/sample/<sample_id>/classification")
 @login_required
-def sample_classification(sample_id, current_user=None):
+def sample_classification(sample_id):
     return render_template('sample_classification.html', sample_id=sample_id, current_user=current_user)
 
 @bp.route("/sample/<sample_id>/nanoplot")
 @login_required
-def sample_nanoplot(sample_id, current_user=None):
+def sample_nanoplot(sample_id):
     return render_template('sample_nanoplot.html', sample_id=sample_id, current_user=current_user)
